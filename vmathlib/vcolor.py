9fa237402f1a78de4f86f137b3effe30e638c91e
wangyueheng <1025067301@qq.com>
2021-06-23 08:29:15 +0800

    Add hsv color, vutil

--- vmathlib/vcolor.py
+++ vmathlib/vcolor.py
@@ -1,8 +1,33 @@
 
+import colorsys
+
 from vmath import Vector3
 from vmathlib import vcolors
 
 MISSING_COLOR = Vector3(1.0, 0.0, 1.0)
 
+BLACK = Vector3(0.0, 0.0, 0.0)
+GRAY = Vector3(0.5, 0.5, 0.5)
+WHITE = Vector3(1.0, 1.0, 1.0)
+RED = Vector3(1.0, 0.0, 0.0)
+GREEN = Vector3(0.0, 1.0, 0.0)
+BLUE = Vector3(0.0, 0.0, 1.0)
+YELLOW = Vector3(1.0, 1.0, 0.0)
+MAGENTA = Vector3(1.0, 0.0, 1.0)
+CYAN = Vector3(0.0, 1.0, 1.0)
+
+
 def named(colorname):
 	return getattr(vcolors, colorname.lower(), MISSING_COLOR)
+
+def hue(h):
+	return Vector3(*colorsys.hsv_to_rgb(h, 1.0, 1.0))
+
+def hsv(h, s, v):
+	return Vector3(*colorsys.hsv_to_rgb(h, s, v))
+
+def bounce(a, b, t):
+	if t > 0.5:
+		t = 1.0 - t
+	t *= 2.0
+	return a + (b - a) * t
--- vmathlib/vutil.py
+++ vmathlib/vutil.py
@@ -7,3 +7,7 @@ VEC3_ONES = vmath.Vector3(1.0, 1.0, 1.0)
 VEC3_X = vmath.Vector3(1.0, 0.0, 0.0)
 VEC3_Y = vmath.Vector3(0.0, 1.0, 0.0)
 VEC3_Z = vmath.Vector3(0.0, 0.0, 1.0)
+
+
+def lerp(a, b, t):
+	return a + (b - a) * t
